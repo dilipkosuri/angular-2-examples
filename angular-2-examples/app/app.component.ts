638c5d17ccf4d387574fcf08124155c9d1ea72b5
DILIP KOSURI <kosuri.dilip@gmail.com>
2017-09-23 21:53:33 +0800

    Creating the first component using angular-2

--- angular-2-examples/app/app.component.ts
+++ angular-2-examples/app/app.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { Homepage } from './homepage.component'
 
 @Component({
   selector: 'my-app',
-  template: '<h1>Hello World</h1>'
+  template: `<h1>Below is some text fetched from another component</h1>
+             <homepage-text></homepage-text>`,
+  directives: [Homepage]
 })
 export class AppComponent { }
