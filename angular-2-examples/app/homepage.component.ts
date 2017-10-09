3eea2a51be39b998901f61fcae048624830284b3
DILIP KOSURI <kosuri.dilip@gmail.com>
2017-10-09 15:36:52 +0800

    Basic Event binding and passing references as input param attributes.

--- angular-2-examples/app/homepage.component.ts
+++ angular-2-examples/app/homepage.component.ts
@@ -3,26 +3,23 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'homepage-text',
     template: `<h3>{{titleMsg}}</h3>
-      <img [src]="imgLink">
-      <div class="myClass"><p>Using styles attribute along with the template</p></div>
-      <div [ngClass]="{myClass: flag}" >The condition is set to true. Hence, the text below is getting displayed.</div>
-      <div [style.background-color]="'yellow'">This is inline styling</div>
-      <div>
-      <div class="default-section" [style]="getStyle()">
-       using a function to return the style. i.e [style]="getStyle()"
-      </div>
-      <div [class.myClass]="showStyle">
-       Using [style.myClass] = "showStyle"
-       </div>
-    </div>`,
+      <img [src]="imgLink"><br>
+      <button class="default-section" (click)="buttonClicked()">Event handling</button>
+      <br>
+      <input class="inputTextBoxStyle" type="text" #inputParam>
+      <button class="default-section" (click)="buttonClickWithPassingAttr(inputParam.value)">Event handling</button>
+    `,
     styles: [`.myClass {
          font-size: 28px;
          font-weight: bold;
          color: pink;
     }
     .default-section {
-      height: 200px;
-      width: 100%;
+      font-size: 28px;
+      font-weight: bold;
+    }
+    .inputTextBoxStyle {
+      border: thick solid #CCC;
     }
     `]  
 })
@@ -37,7 +34,11 @@ export class Homepage{
     constructor() {
     }
     
-    getStyle() {
-      return `background-image: url("http://lorempixel.com/400/200")`;
+    buttonClicked() {
+      console.log("Button is clicked");
+    }
+
+    buttonClickWithPassingAttr(value) {
+        console.log("input value passed through event handling is: \n",value)
     }
 }    
